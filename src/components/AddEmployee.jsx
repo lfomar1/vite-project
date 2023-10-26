4521f1b4dfd2735c182790745b24506b91fac818
Lucas <107668539+lfomar1@users.noreply.github.com>
2023-10-26 00:45:53 -0300

    Some new functionality like add button and edit button

---
+++ src/components/AddEmployee.jsx
@@ -0,0 +1,129 @@
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+
+function AddEmployee(props) {
+  const [name, setName] = useState("");
+  const [role, setRole] = useState("");
+  const [image, setImage] = useState("");
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <button
+        onClick={handleShow}
+        className="max-h-10 m-3	block bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded"
+      >
+        + Add Employee
+      </button>
+
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Add Employee</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <form
+            className="w-full max-w-sm"
+            id="editform"
+            onSubmit={(e) => {
+              e.preventDefault();
+              console.log(props.id, name, role);
+              props.changeEmployeeInfo(props.id, name, role);
+            }}
+          />
+          <div className="md:flex md:items-center mb-6">
+            <div className="md:w-1/3">
+              <label
+                className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                htmlFor="name"
+              >
+                Full Name
+              </label>
+            </div>
+            <div className="md:w-2/3">
+              <input
+                className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                id="name"
+                type="text"
+                placeholder="Juan Alberto"
+                value={name}
+                onChange={(e) => {
+                  setName(e.target.value);
+                }}
+              />
+            </div>
+          </div>
+          <div className="md:flex md:items-center mb-6">
+            <div className="md:w-1/3">
+              <label
+                className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                htmlFor="role"
+              >
+                Role
+              </label>
+            </div>
+            <div className="md:w-2/3">
+              <input
+                className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                id="role"
+                type="text"
+                placeholder="Developer"
+                value={role}
+                onChange={(e) => {
+                  setRole(e.target.value);
+                }}
+              />
+            </div>
+          </div>
+          <div className="md:flex md:items-center mb-6">
+            <div className="md:w-1/3">
+              <label
+                className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                htmlFor="image"
+              >
+                Image URL
+              </label>
+            </div>
+            <div className="md:w-2/3">
+              <input
+                className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                id="image"
+                type="text"
+                placeholder="https://images.pexels.com/photos/2853592/pexels-photo-2853592.jpeg"
+                value={image}
+                onChange={(e) => {
+                  setImage(e.target.value);
+                }}
+              />
+            </div>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <button
+            variant="secondary"
+            onClick={handleClose}
+            className="bg-slate-500 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded"
+          >
+            Close
+          </button>
+          <button
+            form="editform"
+            className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded"
+            onClick={handleClose}
+          >
+            Add
+          </button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
+export default AddEmployee;
--- src/components/Employees.jsx
+++ src/components/Employees.jsx
@@ -1,9 +1,26 @@
+import EditEmployee from "./EditEmployee";
+
 const Employees = (props) => {
   return (
-    <>
-      <h1>Here is a employee name {props.name}</h1>
-      <p>Role: {props.role ? props.role : "No role for this person"}</p>
-    </>
+    <div className="m-2 py-8 px-8 max-w-sm  bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+      <img
+        className="object-cover h-[100px] w-[100px] block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0"
+        src={props.img}
+        alt="Woman's Face"
+      />
+      <div className="text-center space-y-2 sm:text-left">
+        <div className="space-y-0.5">
+          <p className="text-lg text-black font-semibold">{props.name}</p>
+          <p className="text-slate-500 font-medium">{props.role}</p>
+        </div>
+        <EditEmployee
+          id={props.id}
+          name={props.name}
+          role={props.role}
+          changeEmployeeInfo={props.changeEmployeeInfo}
+        />
+      </div>
+    </div>
   );
 };
 export default Employees;
